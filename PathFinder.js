e1a0feaa649410ff4a00ef54dfb7e955bda5e1a5
Recoskie <damian_programmer@hotmail.com>
2014-09-10 00:29:24 -0700

    Update PathFinder.js
    did not need new paths and old paths list only slowed it down also did not need two while loops by putting the new paths at the end of the list and loading the first and by deleting the first after extends made the path finder better next is to implement best first search be reorganizing the lists order by closest paths to goal ad by not deleting older paths and ignoring them makes A*.

--- PathFinder.js
+++ PathFinder.js
@@ -1,3 +1,7 @@
+//************************************list of Visited Path Points*************************************
+
+var PointsVisited=new Array();
+
 //**************************************define the map 2d matrix**************************************
 
 var map=
@@ -14,17 +18,16 @@ var map=
 [1,1,1,0,1,0,1,1,1,1]
 ]
 
-//***************************************display one single path list**************************************
+//***********************************display one single path list*************************************
 
-function DisplayPath(list)
-{var output="";
+function DisplayPath(list){var output="";
 try{for(var i=0;i<list.length;i++)
 {x=list[i][1];y=list[i][0];
 output+="x="+x+",y="+y+"\r\n";}}
 catch(e){return("no possible path");}
 return(output);}
 
-//*****************************display all paths in separated in lists*************************
+//******************************display all paths in separated in lists*******************************
 
 function DisplayPaths(list)
 {var output="";
@@ -35,53 +38,11 @@ for(var i2=0;i2<list[i].length;i2++)
 output+="x="+x+",y="+y+",";}}
 return(output);}
 
-//****************************************check visited pionts****************************************
+//*******************************get new path points that can be moved to*****************************
 
-function NotVisited(vp,cmp)
+function GetMoves(x1,y1)
 {
-for(var i=0;i<vp.length;i++)
-{
-if(vp[i][1]==cmp[1]&vp[i][0]==cmp[0])
-{
-return(false);
-}
-}
-return(true);
-}
-
-//*************************************run path finding algoritham************************************
-
-function FindPath(x1,y1,x2,y2)
-{
-
-//**************************store all points traveled an an fast to read list*************************
-
-var PointsVisited=new Array();
-
-//******************************************new and old paths*****************************************
-
-var original=new Array();
-var newpaths=new Array();
-
-//*****************************************load start point in****************************************
-
-original[original.length]=[[x1,y1]];
-
-//***********************************main loop ends when all paths fail*******************************
-
-while(original.length>0|newpaths.length>0)
-{
-
-//****extract each paths final point and expand foreword into all positions remove the original path****
-
-while(original.length>0)
-{
-//****************************get x and y of an path and continue path forward************************
-
-x1=original[0][original[0].length-1][1];
-y1=original[0][original[0].length-1][0];
-
-if(x1==x2&y1==y2){return(original[0]);} //*******if point to goal is found return the x,y points******
+var output=new Array();
 
 //***********************************check if no wall move down one***********************************
 
@@ -92,13 +53,11 @@ if(y1<map.length-1){if(map[y1+1][x1]==0)
 if(NotVisited(PointsVisited,[y1+1,x1]))
 {
 
-//*****************************add this to an new path and as visited point***************************
+//************************record this point as open move and set it as visited************************
 
 PointsVisited[PointsVisited.length]=[y1+1,x1];
-newpaths[newpaths.length]=original[0].concat([[y1+1,x1]]);
-
+output[output.length]=[y1+1,x1];
 }
-
 }}
 
 //**********************************check if no wall move up one**************************************
@@ -110,13 +69,11 @@ if(y1!=0){if(map[y1-1][x1]==0)
 if(NotVisited(PointsVisited,[y1-1,x1]))
 {
 
-//*****************************add this to an new path and as visited point***************************
+//************************record this point as open move and set it as visited************************
 
 PointsVisited[PointsVisited.length]=[y1-1,x1];
-newpaths[newpaths.length]=original[0].concat([[y1-1,x1]]);
-
+output[output.length]=[y1-1,x1];
 }
-
 }}
 
 //*********************************check if no wall move across one***********************************
@@ -128,48 +85,89 @@ if(x1<map[y1].length){if(map[y1][x1+1]==0)
 if(NotVisited(PointsVisited,[y1,x1+1]))
 {
 
-//*****************************add this to an new path and as visited point***************************
+//************************record this point as open move and set it as visited************************
 
 PointsVisited[PointsVisited.length]=[y1,x1+1];
-newpaths[newpaths.length]=original[0].concat([[y1,x1+1]]);
-
+output[output.length]=[y1,x1+1];
 }
-
 }}
 
 //***********************************check if no wall move back one***********************************
 
-if(x1!=0)
-{
-if(map[y1][x1-1]==0)
+if(x1!=0){if(map[y1][x1-1]==0)
 {
 //**********************check if the point has been previusly visited by another**********************
 
 if(NotVisited(PointsVisited,[y1,x1-1]))
 {
 
-//*****************************add this to an new path and as visited point***************************
+//************************record this point as open move and set it as visited************************
 
 PointsVisited[PointsVisited.length]=[y1,x1-1];
-newpaths[newpaths.length]=original[0].concat([[y1,x1-1]]);
-
+output[output.length]=[y1,x1-1];
 }
-
 }}
 
-//****************************************remove extended path****************************************
+//******************************************return new moves******************************************
 
-original.splice(0,1);} //******************************end of while loop******************************
+return(output);
+}
 
-//*****************************************************************************************************
-//copy the new paths to the original paths and reset the new paths then start expanding the paths again
-//*****************************************************************************************************
+//****************************************check visited pionts****************************************
 
-original=new Array();original=original.concat(newpaths);newpaths=new Array();
+function NotVisited(vp,cmp)
+{
+for(var i=0;i<vp.length;i++)
+{
+if(vp[i][1]==cmp[1]&vp[i][0]==cmp[0])
+{return(false);}
+}
+return(true);
+}
+
+//*************************************run path finding algoritham************************************
+
+function FindPath(x1,y1,x2,y2)
+{
+
+//*********************************************paths list*********************************************
+
+var paths=new Array();
+
+//*****************************************load start point in****************************************
+
+paths[paths.length]=[[x1,y1]];
+
+//***********************************main loop ends when all paths fail*******************************
+
+while(paths.length>0)
+{
+
+//****extract each paths final point and expand foreword into all positions remove the original path****
+
+x1=paths[0][paths[0].length-1][1];
+y1=paths[0][paths[0].length-1][0];
+
+//*****************************if point to goal is found return the x,y points************************
+
+if(x1==x2&y1==y2){PointsVisited=new Array();return(paths[0]);}
+
+//******check which directions this path can move to and then add new points into diffrent paths******
+
+Moves=GetMoves(x1,y1);
+
+for(var i=0;i<Moves.length;i++)
+{
+paths[paths.length]=paths[0].concat([Moves[i]]);
+}
+
+//************************************remove the exstended path***************************************
+
+paths.splice(0,1);
 
 //*************************************display the different paths************************************
 
-WScript.echo("new paths after exstend"+DisplayPaths(original)+"");
+WScript.echo("new paths after exstend"+DisplayPaths(paths)+"");
 
 } //***************************************end of main loop*******************************************
 
